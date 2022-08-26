dbac957619185abce716fd979fe834bec511f882
Fernanda Bastos <100588756+FernandaBast@users.noreply.github.com>
2022-08-25 21:36:07 -0300

    Add files via upload

---
+++ Mundo Invertido/data/firebase.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCPAVZg1a00l8rp7sWG2lgQdlDEe1spzOs",
+    authDomain: "projeto-mundo-invertido-1fa39.firebaseapp.com",
+    projectId: "projeto-mundo-invertido-1fa39",
+    storageBucket: "projeto-mundo-invertido-1fa39.appspot.com",
+    messagingSenderId: "563759338816",
+    appId: "1:563759338816:web:f18c4a66ec81f3f87a96a5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export default firebase;
